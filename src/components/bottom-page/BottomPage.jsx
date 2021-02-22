7c647cf7f2f28ed7106b4a938c3291126faa003b
Samantha Lima Luna Oriá <samanthalimaadv@yahoo.com.br>
2021-02-22 15:00:30 +0100

    added bootstrap

--- src/components/bottom-page/BottomPage.jsx
+++ src/components/bottom-page/BottomPage.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
+
 export const BottomPage = () => (
   <div className="align-link" id="align-link">
     <small className="italic"> </small>
+    <footer> 
     <a
       href="https://github.com/samanthaoria/weather-react.git"
       className="link-git italic bold"
       target="_blank"
     >
-      Open-source code
+      Open-source code 
     </a>
     <span className="name-after-git bold"> by Samantha Oriá</span>
+    </footer>
+    
   </div>
 );
 
